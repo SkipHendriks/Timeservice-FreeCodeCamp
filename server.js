5b2256f12cde0b1094d181523a64609912f47a95
Glitch (strong-bean) <none>
2019-05-01 11:26:12 +0000

    👦🛬 Checkpoint ./server.js:4202090/1414

--- server.js
+++ server.js
@@ -20,69 +20,38 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/:dat", function (req, res) {
+app.get("/api/timestamp/:dat", function (req, res) {
     const request_string = req.params.dat;
-    var is_natural = /^\w{3,7} ?\d{1,2}(, | |,)(\d{2}|\d{4})$/i.test(request_string);
-    var is_unix = /^\d{10}$/.test(request_string);
+    const is_natural = /^\w{3,7} ?\d{1,2}(, | |,)(\d{2}|\d{4})$/i.test(request_string);
+    const is_unix = /^\d{10}$/.test(request_string);
     
     if ((!is_natural && !is_unix) || request_string.length === 0) {
         res.writeHead(422, {'Content-type': 'text/plain'});
         res.end("Unrecognized natural language or unix time format");
     }
   
-    res.writeHead(200, {'Content-type': 'text/plain'});
-    const return_object = {};
+    let res_object = {};
     if (is_natural) {
-        return_object.natural = request_string;
-        return_object.unix = getUnix(request_string);
+        res_object.natural = request_string;
+        res_object.unix = getUnix(request_string);
+    } else {
+        res_object.unix = request_string;
+        res_object.natural = parseNatural(request_string);
     }
-    if (is_unix) {
-        res.end(parseJSON(undefined, path));
-    }
-});
-
-var server = http.createServer(function(req, res) {
-    var path = require('url').parse(req.url).path;
-    path = path.slice(13, path.length);
-    path = decodeURI(path);
-    var is_natural = /^\w{3,7} ?\d{1,2}(, | |,)(\d{2}|\d{4})$/i.test(path);
-    var is_unix = /^\d{10}$/.test(path);
-
-    if ((!is_natural && !is_unix) || path.length === 0) {
-        res.writeHead(422, {'Content-type': 'text/plain'});
-        res.end("Unrecognized natural language or unix time format");
-    }
-
     res.writeHead(200, {'Content-type': 'text/plain'});
-    if (is_natural) {
-        const natural =  
-    }
-    if (is_unix) {
-        res.end(parseJSON(undefined, path));
-    }
+    res.end(JSON.stringify(res_object));
 });
-server.listen(8081);
-
-function parseJSON(natural, unix) {
-    if(natural === undefined) {
-        natural = parseNatural(unix);
-    }
-    if(unix === undefined) {
-        unix = parseUnix(natural);
-    }
-    return JSON.stringify({'unix': unix, 'natural': natural});
-}
 
 function getUnix (natural) {
-    var date = new Date(natural);
+    const date = new Date(natural);
     return date.getTime() / 1000;
 }
 
 function parseNatural(unix) {
-    var monthNames = ["January", "February", "March", "April", "May", "June",
+    const monthNames = ["January", "February", "March", "April", "May", "June",
         "July", "August", "September", "October", "November", "December"];
-    var date = new Date(unix * 1000);
-    var string = monthNames[date.getMonth()] + " ";
+    const date = new Date(unix * 1000);
+    let string = monthNames[date.getMonth()] + " ";
     string += date.getDate() + ", ";
     string += date.getFullYear();
     return string;
