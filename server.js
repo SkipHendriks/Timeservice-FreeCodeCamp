b8e1a4e395f70dc7c09b028ff8db106424799f59
Glitch (strong-bean) <none>
2019-05-01 10:53:16 +0000

    🐓🍚 Checkpoint ./server.js:4202090/1810

--- server.js
+++ server.js
@@ -20,10 +20,62 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
+app.get("/api/:", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+
+var http = require('http');
+
+var server = http.createServer(function(req, res) {
+    var path = require('url').parse(req.url).path;
+    path = path.slice(13, path.length);
+    path = decodeURI(path);
+    var is_natural = /^\w{3,7} ?\d{1,2}(, | |,)(\d{2}|\d{4})$/i.test(path);
+    var is_unix = /^\d{10}$/.test(path);
+
+    if (is_natural === true && is_unix === true) {
+        throw new RegexError("Both expressions match. Path: " + path);
+    }
+    if ((!is_natural && !is_unix) || path.length === 0) {
+        res.writeHead(422, {'Content-type': 'text/plain'});
+        res.end("Unrecognized natural language or unix time format");
+    }
+
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    if (is_natural) {
+        res.end(parseJSON(path, undefined));
+    }
+    if (is_unix) {
+        res.end(parseJSON(undefined, path));
+    }
+});
+server.listen(8081);
+
+function parseJSON(natural, unix) {
+    if(natural === undefined) {
+        natural = parseNatural(unix);
+    }
+    if(unix === undefined) {
+        unix = parseUnix(natural);
+    }
+    return JSON.stringify({'unix': unix, 'natural': natural});
+}
+
+function parseUnix (natural) {
+    var date = new Date(natural);
+    return date.getTime() / 1000;
+}
+
+function parseNatural(unix) {
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"];
+    var date = new Date(unix * 1000);
+    var string = monthNames[date.getMonth()] + " ";
+    string += date.getDate() + ", ";
+    string += date.getFullYear();
+    return string;
+}
 
 
 // listen for requests :)
